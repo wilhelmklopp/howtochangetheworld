6aaa34ff78aa0f68fb088922d5ecc0abf579ee99
Wilhelm Klopp <wil.klopp@gmail.com>
2017-05-27 14:39:44 +0100

    Add allowed host

--- server/htctwserver/settings.py
+++ server/htctwserver/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DEBUG"] == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://htctw-server.herokuapp.com/']
 
 
 # Application definition
