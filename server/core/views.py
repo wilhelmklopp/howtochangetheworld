c46b112c55e824e35407d9699486ca8ce4800cc8
Wilhelm Klopp <wil.klopp@gmail.com>
2017-05-27 15:02:44 +0100

    debug cookie problem

--- server/core/views.py
+++ server/core/views.py
@@ -256,6 +256,7 @@ def ml_exchange(request):
 
 def my_group(request):
     try:
+        print request.COOKIES
         access_token = request.COOKIES["accessToken"]
     except KeyError:
         response = JsonResponse({
